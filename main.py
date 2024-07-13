fc2ddc2f6e96c875435aace2b553f5b522fd8d0c
Fikri Choiri Ahmadi <fahmadi@appen.com>
2024-07-13 17:26:15 +0700

    1st revision

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # def mainMenu():
-
+from aritmatika import bangunRuang, bangunDatar, interface_kubus, interface_karucut, interface_tabung, interface_balok
 
 
 
@@ -13,7 +13,7 @@ def mainMenu():
       6. Exit''')
     
     pilihan = int(input("Masukkan pilihan anda: "))
-    if pilihan == '1':
+    if pilihan == 1:
         integral()
     elif pilihan == 2:
         turunan()
@@ -30,17 +30,6 @@ def mainMenu():
         mainMenu()
 
 
-
-
-
-def integral():
-    pass
-
-
-def turunan():
-    pass
-
-
 def aritmatika():
     print('''Selamat datang di program Python 
       1. Bangun Ruang
@@ -54,9 +43,6 @@ def aritmatika():
         bangunDatar()
     elif pilihan == 3:
         operasiAritmatika()
-        
-
-## put ALL the function below here
 
 def bangunRuang():
     while True:
@@ -68,7 +54,7 @@ def bangunRuang():
             0. Main menu
             ''')
         pilihan = int(input("Masukkan operasi yang anda inginkan: "))
-        
+
         if pilihan == 1:
             interface_kubus()
         elif pilihan == 2:
@@ -82,6 +68,90 @@ def bangunRuang():
         else:
             print("Menu tidak ada pada pilihan, masukkan 0 - 4")
         ValueError("masukan Angka")
+
+
+
+
+def integral():
+    while True:
+        print('''Silahkan pilih jenis integral: 
+        1. Integral Tentu
+        2. Integral Tak Tentu''')
+        pilihan = int(input('Masukkan operasi integral: '))
+        if pilihan == 1:
+            integralTentu()
+        elif pilihan == 2:
+            integralTakTentu()
+
+
+
+
+
+def turunan():
+    pass
+
+
+def integralTentu():
+    base = int(input('masukkan base number: '))
+    power = int(input('masukkan power number: '))
+    result = base**power
+
+    test = base*('x')
+
+    # integralEquation = (base/power+1)*(base)*
+
+    print(test)
+
+
+
+
+
+
+def integralTakTentu():
+    pass
+
+
+# def aritmatika():
+#     print('''Selamat datang di program Python
+#       1. Bangun Ruang
+#       2. Bangun Datar
+#       3. Operasi Aritmatika
+#     ''')
+#     pilihan = int(input("Masukkan operasi yang anda inginkan: "))
+#     if pilihan == 1:
+#         bangunRuang()
+#     elif pilihan == 2:
+#         bangunDatar()
+#     elif pilihan == 3:
+#         operasiAritmatika()
+        
+
+## put ALL the function below here
+
+# def bangunRuang():
+#     while True:
+#         print('''Menu Bangun Ruang
+#             1. Kubus
+#             2. Balok
+#             3. Tabung
+#             4. Kerucut
+#             0. Main menu
+#             ''')
+#         pilihan = int(input("Masukkan operasi yang anda inginkan: "))
+#
+#         if pilihan == 1:
+#             interface_kubus()
+#         elif pilihan == 2:
+#             interface_balok()
+#         elif pilihan == 3:
+#             interface_tabung()
+#         elif pilihan == 4:
+#             interface_karucut()
+#         elif pilihan == 0:
+#             mainMenu()
+#         else:
+#             print("Menu tidak ada pada pilihan, masukkan 0 - 4")
+#         ValueError("masukan Angka")
 
 def operasiAritmatika():
     pass
@@ -101,76 +171,76 @@ def translate():
 
 
 
-def interface_kubus():
-    while True:
-        s = float(input("Masukkan sisi : "))
-        if s > 0:
-            print(f"Luas kubus adalah : {calculate_kubus(s)}")
-            break
-        else:
-            print("input yang dimasukkan tidak sesuai")
-        ValueError("Masukan input dalam angka")
-        
-
-def interface_balok():
-    while True:
-        p = float(input("Masukkan panjang balok : "))
-        l = float(input("Masukkan lebar balok : "))
-        t = float(input("Masukkan tinggi balok : "))
-        
-        if p > 0 and l > 0 and t > 0 :
-            print(f"Luas balok adalah {calculate_balok(p,l,t)}")
-            break
-        else:
-            print("input yang dimasukkan3 tidak sesuai")
-        ValueError("Masukan input dalam angka")
-
-def interface_tabung():
-    while True:
-        r = float(input("Masukkan Jari -jari : "))
-        t = float(input("Masukkan tinggi tabung : "))
-        
-        if r > 0 and t > 0:
-             print(f"Luas tabung adalah {calculate_tabung(r,t)}")
-             break
-        else:
-            print("input yang dimasukkan tidak sesuai")
-        ValueError("Masukan input dalam angka")
-            
-        
-def interface_karucut():
-    while True:
-        r = float(input("Masukkan Jari -jari : "))
-        t = float(input("Masukkan tinggi tabung : "))
-        
-        if r > 0  and t > 0:
-             print(f"Luas kerucut adalah {calculate_kerucut(r,t)}")
-             break
-        else:
-            print("input yang dimasukkan tidak sesuai")
-        ValueError("Masukan input dalam angka")       
-    
-
-def calculate_kubus(s):
-    luas_kubus = s ** 3
-    return(luas_kubus)
-
-def calculate_balok(p,l,t):
-    luas_balok = p * l * t
-    return(luas_balok)
-
-def calculate_tabung(r,t):
-    v = 3.14
-    luas_tabung= v*r*r*t
-    return(luas_tabung)
-
-def calculate_kerucut(r,t):
-    v = 3.14
-    a = 1/3
-    luas_kerucut = a * v * r * r *t
-    return(luas_kerucut)
-
-
+# def interface_kubus():
+#     while True:
+#         s = float(input("Masukkan sisi : "))
+#         if s > 0:
+#             print(f"Luas kubus adalah : {calculate_kubus(s)}")
+#             break
+#         else:
+#             print("input yang dimasukkan tidak sesuai")
+#         ValueError("Masukan input dalam angka")
+#
+#
+# def interface_balok():
+#     while True:
+#         p = float(input("Masukkan panjang balok : "))
+#         l = float(input("Masukkan lebar balok : "))
+#         t = float(input("Masukkan tinggi balok : "))
+#
+#         if p > 0 and l > 0 and t > 0 :
+#             print(f"Luas balok adalah {calculate_balok(p,l,t)}")
+#             break
+#         else:
+#             print("input yang dimasukkan3 tidak sesuai")
+#         ValueError("Masukan input dalam angka")
+#
+# def interface_tabung():
+#     while True:
+#         r = float(input("Masukkan Jari -jari : "))
+#         t = float(input("Masukkan tinggi tabung : "))
+#
+#         if r > 0 and t > 0:
+#              print(f"Luas tabung adalah {calculate_tabung(r,t)}")
+#              break
+#         else:
+#             print("input yang dimasukkan tidak sesuai")
+#         ValueError("Masukan input dalam angka")
+#
+#
+# def interface_karucut():
+#     while True:
+#         r = float(input("Masukkan Jari -jari : "))
+#         t = float(input("Masukkan tinggi tabung : "))
+#
+#         if r > 0  and t > 0:
+#              print(f"Luas kerucut adalah {calculate_kerucut(r,t)}")
+#              break
+#         else:
+#             print("input yang dimasukkan tidak sesuai")
+#         ValueError("Masukan input dalam angka")
+#
+#
+# def calculate_kubus(s):
+#     luas_kubus = s ** 3
+#     return(luas_kubus)
+#
+# def calculate_balok(p,l,t):
+#     luas_balok = p * l * t
+#     return(luas_balok)
+#
+# def calculate_tabung(r,t):
+#     v = 3.14
+#     luas_tabung= v*r*r*t
+#     return(luas_tabung)
+#
+# def calculate_kerucut(r,t):
+#     v = 3.14
+#     a = 1/3
+#     luas_kerucut = a * v * r * r *t
+#     return(luas_kerucut)
+#
+#
 mainMenu()
 
 def exit():
