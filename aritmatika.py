2787971e24d8fe848541c38931632cbcc379759e
Ujeeg <fajaraarrahman11@gmail.com>
2024-07-13 18:25:50 +0700

    add new feature y/n

--- aritmatika.py
+++ aritmatika.py
@@ -1,53 +1,65 @@
 # from main import mainMenu
 def interface_kubus():
     while True:
-        s = float(input("Masukkan sisi : "))
-        if s > 0:
-            print(f"Luas kubus adalah : {calculate_kubus(s)}")
-            break
-        else:
-            print("input yang dimasukkan tidak sesuai")
-        ValueError("Masukan input dalam angka")
+        try :
+            s = float(input("Masukkan sisi : "))
+            if s > 0:
+                print(f"Luas kubus adalah : {calculate_kubus(s)}")
+                if not yes_or_no_interface():
+                    break 
+            else:
+                print("input yang dimasukkan tidak sesuai")
+        except ValueError:
+            print("Masukan input dalam angka")
 
 
 def interface_balok():
     while True:
-        p = float(input("Masukkan panjang balok : "))
-        l = float(input("Masukkan lebar balok : "))
-        t = float(input("Masukkan tinggi balok : "))
+        try :
+            p = float(input("Masukkan panjang balok : "))
+            l = float(input("Masukkan lebar balok : "))
+            t = float(input("Masukkan tinggi balok : "))
 
-        if p > 0 and l > 0 and t > 0:
-            print(f"Luas balok adalah {calculate_balok(p, l, t)}")
-            break
-        else:
-            print("input yang dimasukkan3 tidak sesuai")
-        ValueError("Masukan input dalam angka")
+            if p > 0 and l > 0 and t > 0:
+                print(f"Luas balok adalah {calculate_balok(p, l, t)}")
+                if not yes_or_no_interface():
+                    break
+            else:
+                print("input yang dimasukkan3 tidak sesuai")
+        except ValueError:
+            print("Masukan input dalam angka")
 
 
 def interface_tabung():
     while True:
-        r = float(input("Masukkan Jari -jari : "))
-        t = float(input("Masukkan tinggi tabung : "))
+        try :
+            r = float(input("Masukkan Jari -jari : "))
+            t = float(input("Masukkan tinggi tabung : "))
 
-        if r > 0 and t > 0:
-            print(f"Luas tabung adalah {calculate_tabung(r, t)}")
-            break
-        else:
-            print("input yang dimasukkan tidak sesuai")
-        ValueError("Masukan input dalam angka")
+            if r > 0 and t > 0:
+                print(f"Luas tabung adalah {calculate_tabung(r, t)}")
+                if not yes_or_no_interface():
+                    break 
+            else:
+                print("input yang dimasukkan tidak sesuai")
+        except ValueError:
+            print("Masukan input dalam angka")
 
 
 def interface_karucut():
     while True:
-        r = float(input("Masukkan Jari -jari : "))
-        t = float(input("Masukkan tinggi tabung : "))
+        try :
+            r = float(input("Masukkan Jari -jari : "))
+            t = float(input("Masukkan tinggi tabung : "))
 
-        if r > 0 and t > 0:
-            print(f"Luas kerucut adalah {calculate_kerucut(r, t)}")
-            break
-        else:
-            print("input yang dimasukkan tidak sesuai")
-        ValueError("Masukan input dalam angka")
+            if r > 0 and t > 0:
+                print(f"Luas kerucut adalah {calculate_kerucut(r, t)}")
+                if not yes_or_no_interface():
+                    break 
+            else:
+                print("input yang dimasukkan tidak sesuai")
+        except ValueError:
+            print("Masukan input dalam angka")
 
 
 def calculate_kubus(s):
@@ -79,3 +91,18 @@ def bangunDatar():
 
 def operasiAritmatika():
     pass
+
+
+def yes_or_no_interface():
+    while True:
+        try :
+            print("Apakah ada akan menghitung kembali (y/n) :")
+            pilihan = input("Masukan Input : ").strip().lower()
+            if pilihan == "y":
+                return True
+            elif pilihan == "n":
+                return False
+            else:
+                print("masukan y / n")
+        except ValueError:
+            print("Masukan y / n")
